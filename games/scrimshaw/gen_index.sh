4fccafb069da42ea081a05c9e40a02f079738b33
Phildo <phildo211@gmail.com>
2016-06-13 12:10:26 -0500

    basic, functional index generation

--- games/scrimshaw/gen_index.sh
+++ games/scrimshaw/gen_index.sh
@@ -3,8 +3,12 @@
 cat index_top.html.part > index.html;
 
 find ./media -type f -print | sed 's@.*/@@' | sort -n | while read file ; do
-  echo "<img class=\"screen\" src=\"media/"$file"\" />" >> index.html
-done;
+  if [[ ! $file =~ (.gif|.png) ]] ; then
+    echo "<a href=\"media/"$file"\"><img class=\"screen\" src=\"assets/media_play.png\" /></a>" >> index.html
+  else
+    echo "<a href=\"media/"$file"\"><img class=\"screen\" src=\"media/"$file"\" /></a>" >> index.html
+  fi
+done
 
 cat index_bottom.html.part >> index.html
 
