8f5488c1c253f25d52e1fd3fc7028234990981dc
Phildo <phildo211@gmail.com>
2016-09-08 18:00:22 -0500

    gen'd new index from gifs

--- games/scrimshaw/gen_previews.sh
+++ games/scrimshaw/gen_previews.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find . -type f | sed 's@.*/@@' | while read file ; do
-  if [[ $file =~ .gif ]] ; then
+  if [[ $file =~ .gif ]] && [[ ! -f media/preview/$file ]]; then
     echo $file
     convert media/$file[0-20] -coalesce -resize 400x100 -layers OptimizeFrame media/preview/$file
   fi
